6460150e4566bc9786034c2ef2f71034d7395eb3
minire <minichre@gmail.com>
2013-07-02 18:32:57 -0700

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -5,10 +5,10 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
 # Uncommet to configure in file.
-ACCOUNT_SID = "AC11759b18e54ca436f11ac4fc2dbf07db" 
-AUTH_TOKEN = "884317e08c7a43afc8c50038ba8c2259"
-SONYA_APP_SID = "APd3c4f0a1cd8d4a64afa38d5075f10bcc"
-SONYA_CALLER_ID = "12696723560"
+ACCOUNT_SID = "ACnnnnnnnnnnnnnnnnnnnnnnnnnnnnn" 
+AUTH_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxx"
+SONYA_APP_SID = "APcccccccccccc"
+SONYA_CALLER_ID = "12456789032"
 
 
 # Begin Heroku configuration - configured through environment variables.
